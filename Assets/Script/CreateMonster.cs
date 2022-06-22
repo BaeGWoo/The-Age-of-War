9562f4109026b47cb267f2ebc86fa79b906cf173
BaeGWoo <gwbae4211@naver.com>
2022-06-22 22:04:44 +0900

    The Age of War

---
+++ Assets/Script/CreateMonster.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateMonster : MonoBehaviour
+{
+
+   
+
+    public void Create()
+    {
+        Instantiate
+            (
+            Resources.Load<GameObject>("Warrior Goblin"),
+            new Vector3(-28,0,50),
+            Quaternion.Euler(0,90,0)
+            );
+
+
+    }
+}
